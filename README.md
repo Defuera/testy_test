b77387734faa9cfc045257f071c06b2e1de1f7c3
Denis Volyntsev <denis.volyntsev@nl.abnamro.com>
2019-07-10 15:43:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+The assignment:
+
+Create an app that is capable of visualizing the attached csv file on the screen.
+parse it on a background thread
---
+++ app/src/main/java/com/justd/rabo/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.justd.testpr.list
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.justd.rabo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
